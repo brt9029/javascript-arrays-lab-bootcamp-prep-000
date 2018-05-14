b5753978dba39d7558338f6dcf9b1b4f4447b03c
Bryant Torres <brt9029@gmail.com>
2018-05-14 22:50:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name){
-  return kittens.shift(name)
+  return kittens.unshift(name)
 }
 function appendKitten(name){
   return [...kittens, name]
